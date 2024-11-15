ed75fbb43146f31f036f65b0e38bcec3beae94ae
GloRRian55 <GloRRian55@yandex.ru>
2024-11-15 17:38:36 +0300

    check

--- src/main/java/ru/bivchallenge/Application.java
+++ src/main/java/ru/bivchallenge/Application.java
@@ -3,7 +3,7 @@ package ru.bivchallenge;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
+    // check merge rules
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
