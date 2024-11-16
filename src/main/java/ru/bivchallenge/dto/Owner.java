7bee5590c6177b8b2cb2f1a83740bea6ed911139
GloRRian55 <GloRRian55@yandex.ru>
2024-11-17 01:41:41 +0300

    write javadoc

--- src/main/java/ru/bivchallenge/dto/Owner.java
+++ src/main/java/ru/bivchallenge/dto/Owner.java
@@ -1,5 +1,18 @@
 package ru.bivchallenge.dto;
 
+/**
+ * The {@code Owner} interface represents an entity that owns or is associated with a company.
+ * It provides a method to retrieve the company ID of the associated company.
+ *
+ * <p>Implementing classes are expected to define how they are associated with a company,
+ * for example, as a legal entity, natural entity, or other ownership type.</p>
+ */
 public interface Owner {
+
+    /**
+     * Returns the ID of the company associated with this owner.
+     *
+     * @return the ID of the associated company
+     */
     long getCompanyId();
 }
--- src/main/java/ru/bivchallenge/dto/WeightedEdge.java
+++ src/main/java/ru/bivchallenge/dto/WeightedEdge.java
@@ -2,17 +2,42 @@ package ru.bivchallenge.dto;
 
 import org.jgrapht.graph.DefaultEdge;
 
+/**
+ * The {@code WeightedEdge} class represents an edge in a graph with an associated weight.
+ * It extends the {@link DefaultEdge} class from the JGraphT library, allowing it to store
+ * additional data, such as the weight of the edge.
+ *
+ * <p>Weights are typically used to represent the strength, cost, or percentage associated
+ * with the edge in the graph.</p>
+ *
+ * @see DefaultEdge
+ */
 public class WeightedEdge extends DefaultEdge {
     private double weight;
 
+    /**
+     * Constructs a {@code WeightedEdge} with the specified weight.
+     *
+     * @param weight the weight of the edge
+     */
     public WeightedEdge(double weight) {
         this.weight = weight;
     }
 
+    /**
+     * Returns the weight of the edge.
+     *
+     * @return the weight of the edge
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Sets the weight of the edge to the specified value.
+     *
+     * @param weight the new weight of the edge
+     */
     public void setWeight(double weight) {
         this.weight = weight;
     }
