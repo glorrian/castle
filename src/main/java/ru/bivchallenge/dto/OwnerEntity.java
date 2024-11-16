0e89bc44da4b322cd3a220e2f4eda5741de13033
GloRRian55 <GloRRian55@yandex.ru>
2024-11-17 01:41:41 +0300

    write javadoc

--- src/main/java/ru/bivchallenge/dto/OwnerEntity.java
+++ src/main/java/ru/bivchallenge/dto/OwnerEntity.java
@@ -1,4 +1,19 @@
 package ru.bivchallenge.dto;
 
+/**
+ * The {@code OwnerEntity} interface represents an entity that both has a unique identifier
+ * and is associated with a company. It extends the {@link Entity} and {@link Owner} interfaces,
+ * combining their functionalities.
+ *
+ * <p>Classes implementing this interface must provide implementations for:
+ * <ul>
+ *   <li>{@code id()} from {@link Entity} - to return the unique identifier of the entity.</li>
+ *   <li>{@code getCompanyId()} from {@link Owner} - to return the associated company ID.</li>
+ * </ul>
+ * </p>
+ *
+ * @see Entity
+ * @see Owner
+ */
 public interface OwnerEntity extends Entity, Owner {
 }
